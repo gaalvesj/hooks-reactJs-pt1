88096d47ec3f7217e418d185d0affd4463bcd25a
Gabriel <gabrielalves3654@gmail.com>
2022-09-16 19:39:34 -0300

    segunda parte do exercicio feita

--- vite-project/src/Componentes/Form.jsx
+++ vite-project/src/Componentes/Form.jsx
@@ -1,19 +1,25 @@
 import React, {useState} from 'react'
 
 export default function Form() {
-    const [name, setName] = useState('')
-
+    const [name, setName] = useState('');
+    const [years, setYear] = useState(0);
 
   return (
     <form>
         <fieldset>
             <legend>Dados Pessoais</legend>
-            <label htmlFor='name'>Nome Completo</label>
+            <label htmlFor='name'>Nome Completo: </label>
             <input 
             type='text'
             id='name'
             value={name}
             onChange={ ({ target }) => setName(target.value)} />
+            <label htmlFor='idade'> idade: </label>
+            <input 
+            type="number" 
+            id='number'
+            value={years}
+            onChange= {({ target }) => setYear(target.value)} />
         </fieldset>
     </form>
   )
