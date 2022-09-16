bacf9f68bf96ea7fa2199ca6edff850894665d1d
Gabriel <gabrielalves3654@gmail.com>
2022-09-16 19:41:51 -0300

    adicionando a terceira parte

--- vite-project/src/Componentes/Form.jsx
+++ vite-project/src/Componentes/Form.jsx
@@ -3,6 +3,7 @@ import React, {useState} from 'react'
 export default function Form() {
     const [name, setName] = useState('');
     const [years, setYear] = useState(0);
+    const [city, setCity] = useState('');
 
   return (
     <form>
@@ -20,6 +21,12 @@ export default function Form() {
             id='number'
             value={years}
             onChange= {({ target }) => setYear(target.value)} />
+            <label htmlFor='cidade'> cidade: </label>
+            <input 
+            type='text'
+            id='cidade'
+            value={city}
+            onChange={ ({ target }) => setCity(target.value)} />
         </fieldset>
     </form>
   )
